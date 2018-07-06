df67f86e25a09cd05f81a2cb3cd3db92e5a71a87
aayusharora <aayush113002@gmail.com>
2018-07-06 14:28:06 +0530

    added boilerplate

--- Lecture-11/public/app.js
+++ Lecture-11/public/app.js
@@ -1,11 +1,27 @@
 
+
+// Call the function at the top, display()
+
+/*
+* function display() {
+*
+*   Get Request at /display route
+*   Loop that todoList and append on the page
+* }
+ *
+* */
+
+
 function makeRequest() {
+
+    // Send the todo item
+
     $.ajax({
         url: '/add',
         method: 'GET',
         success: function(data) {
             console.log(data);
-
+            // Append TodoList Item on Page
         }
     })
 
--- Lecture-11/server.js
+++ Lecture-11/server.js
@@ -3,11 +3,22 @@ const server = require('express');
 const app = server();
 const PORT = process.env.PORT || 5000;
 
+
+// Create TodoList Array Here
 app.use('/', server.static('./public'));
 
 app.get('/add', function(req,res) {
   res.send("Task Added");
+   // Get the todoItem using req.query
+  // Push In todoList ARRAY
+    // Response back => req.query
 });
+
+app.get('/display', function(req,res) {
+   // Send TodoList Array to the client
+
+})
+
 
 app.listen(PORT, function(){
    console.log("Server running on Port " + PORT);
