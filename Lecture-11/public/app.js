6740251cd7e7e691c98512928d58083752cc9fe2
aayusharora <aayush113002@gmail.com>
2018-07-06 15:40:13 +0530

    Added TODO code

--- Lecture-11/public/app.js
+++ Lecture-11/public/app.js
@@ -1,4 +1,44 @@
 
+$(document).ready(function(){
+    let inp = $('#inp');
+    let result = $('#result');
+    let btn = $('#btn');
+    display();
+    btn.click(function(){
+        makeRequest();
+
+    });
+
+    function makeRequest() {
+
+        // Send the todo item
+
+        $.ajax({
+            url: '/add',
+            method: 'GET',
+            data: {todo: inp.val()},
+            success: function(data) {
+                result.append(`<li>${data}</li>`)
+                // Append TodoList Item on Page
+            }
+        })
+
+    }
+
+    function display(){
+        $.ajax({
+            url: '/display',
+            method: 'GET',
+            success: function(data) {
+                data.forEach(function(i){
+                    result.append(`<li>${i}</li>`)
+                })
+            }
+        })
+
+    }
+});
+
 
 // Call the function at the top, display()
 
@@ -11,19 +51,4 @@
  *
 * */
 
-
-function makeRequest() {
-
-    // Send the todo item
-
-    $.ajax({
-        url: '/add',
-        method: 'GET',
-        success: function(data) {
-            console.log(data);
-            // Append TodoList Item on Page
-        }
-    })
-
-}
 
--- Lecture-11/server.js
+++ Lecture-11/server.js
@@ -3,20 +3,24 @@ const server = require('express');
 const app = server();
 const PORT = process.env.PORT || 5000;
 
-
+let todoList = [];
 // Create TodoList Array Here
 app.use('/', server.static('./public'));
 
 app.get('/add', function(req,res) {
-  res.send("Task Added");
+   let todo = req.query.todo;
+
+
    // Get the todoItem using req.query
   // Push In todoList ARRAY
+    todoList.push(todo);
     // Response back => req.query
+    res.send(todo);
 });
 
 app.get('/display', function(req,res) {
    // Send TodoList Array to the client
-
+    res.send(todoList);
 })
 
 
