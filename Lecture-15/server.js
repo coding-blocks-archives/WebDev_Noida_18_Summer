79178acc644ca0de095ebb37bb05789e8501832c
aayusharora <aayush113002@gmail.com>
2018-07-16 16:56:07 +0530

    Added colors

--- Lecture-15/server.js
+++ Lecture-15/server.js
@@ -2,18 +2,24 @@ const server = require('express');
 const path = require('path');
 const app = server();
 const bodyParser = require('body-parser');
-
+const todo = [];
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,"views"));
 
+app.use('/', server.static('public'));
 
-app.post('/all',function(req,res){
+app.post('/data', function(req, res){
+     todo.push(req.body.color);
+     res.sendStatus(200);
+});
 
-    console.log(req.body.items);
-    res.render('index', {items: req.body.items}, function(err, html) {
+app.get('/all',function(req,res){
+
+    console.log(todo);
+    res.render('main', {items: todo[todo.length - 1]}, function(err, html) {
         res.send(html);
 
     });
